7fcdc1ffe243f73f46e1cd9127a0981aa27b3757
lmantovan222 <l.mantovan222@gmail.com>
2026-02-25 15:40:03 +0100

    JS :  creata funzione popolamento tabella prodotti

--- script.js
+++ script.js
@@ -21,5 +21,81 @@
  * - Aggiungi un filtro per categoria (dropdown) per mostrare solo i prodotti di una certa categoria
  */
 
+let URL_BASE = "http://192.168.1.131:5000/api/products";
+let nomeProdotto = document.getElementById("prdottoNome");
+let immagine = document.getElementById("prodottoImmagine");
+let descrizione = document.getElementById("prodottoDescrizione");
+let prezzo = document.getElementById("prodottoDescrizione");
+let disponibilità = document.getElementById("prodottoDescrizione");
+let tabella = document.getElementById("tabellaProdotti")
 
+function handleError(message) {
+    tabella.innerHTML = '';
 
+    let div = document.createElement('div');
+    div.className = 'error';
+
+    let strong = document.createElement('strong');
+    strong.textContent = `❌ ${message}`;
+
+    div.appendChild(strong);
+    tabella.appendChild(div);
+    console.error('Errore:', message);
+}
+
+async function creaRigheProdotti(prodotti) {
+    try {
+        let risposta = await fetch(`${URL_BASE}`);
+        if (!risposta.ok) {
+            handleError("errore");
+            return;
+        }
+        let prodotti = await risposta.json();
+
+        for (let prodotto of prodotti) {
+
+            let rigaProdotto = document.createElement("tr");
+
+            let cellaImmagine = document.createElement("td");
+            let immagine = document.createElement("img");
+            immagine.src = prodotto.immagine;
+            cellaImmagine.appendChild(immagine);
+
+            let cellaNome = document.createElement("td");
+            cellaNome.textContent = prodotto.nome;
+
+            //let cellaDescrizione = document.createElement("td");
+            //cellaDescrizione.textContent = prodotto.descrizione;
+
+            let cellaPrezzo = document.createElement("td");
+            cellaPrezzo.textContent = prodotto.prezzo;
+
+            let cellaDisponibilita = document.createElement("td");
+            cellaDisponibilita.textContent = prodotto.disponibilita
+            if(prodotto.disponibilita === true){
+                cellaDisponibilita.textContent = "✅"
+            }
+            else{
+                cellaDisponibilita.textContent = "❌"
+            }
+
+            rigaProdotto.appendChild(cellaImmagine);
+            rigaProdotto.appendChild(cellaNome);
+            //rigaProdotto.appendChild(cellaDescrizione);
+            rigaProdotto.appendChild(cellaPrezzo);
+            rigaProdotto.appendChild(cellaDisponibilita);
+
+            tabella.appendChild(rigaProdotto);
+        }
+    } catch { handleError("errore") }
+}
+
+creaRigheProdotti()
+
+function mostraProdotti(prodotti) {
+
+}
+
+function toggleModal() {
+
+}
